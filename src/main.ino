e499a57c47d264ee73a84ed80a3f2768e443249b
Dan Hartman <dhartman@starfireindustries.local>
2019-03-15 16:42:23 -0500

    Remove unneccesary empty lines

--- src/main.ino
+++ src/main.ino
@@ -16,14 +16,12 @@ void setup() {
   digitalWrite(10, HIGH);
   digitalWrite(11, HIGH);
 
-
   // Setup the button with an internal pull-up :
   pinMode(BUTTON_PIN,INPUT_PULLUP);
 
   // After setting up the button, setup the Bounce instance :
   debouncer.attach(BUTTON_PIN);
   debouncer.interval(25); // interval in ms
-
 
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
@@ -35,8 +33,6 @@ void setup() {
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
-
-
 }
 
 int index = 2;
